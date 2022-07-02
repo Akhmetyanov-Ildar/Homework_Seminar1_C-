e0f0549055709ba5dc412130fd0cf6160f4190b6
Ildar <example.ru>
2022-07-03 00:46:52 +0500

    fixed Task019

--- Task019/Program.cs
+++ Task019/Program.cs
@@ -7,10 +7,18 @@
 Console.Clear();
 Console.WriteLine("введите пятизначное число");
 int number = int.Parse(Console.ReadLine());
-if (number < 10000 || number > 99000)
+if (number < 10_000 || number > 99_999)
 {
     Console.Write("Вы ввели не пятизначное число");
 }
-int pal = 0, num = number;
-while (number > 0) { pal = pal * 10 + number % 10; number /= 10; }
-Console.WriteLine(num == pal ? "палиндром" : "не палиндром");
+else
+{
+    int pal = 0, num = number;
+    while (number > 0)
+    {
+        pal = pal * 10 + number % 10;
+        number /= 10;
+    }
+    Console.WriteLine(num == pal ? "палиндром" : "не палиндром");
+}
+
