dd0cf535dbfa7788f555915dec4959bbb6bfe069
Ildar <example.ru>
2022-07-02 23:18:55 +0500

    Task023

---
+++ Task023/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+Console.Clear();
+Console.WriteLine("Введите  число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+int num = int.Parse(Console.ReadLine());
+int i = 1;
+while (i <= num)
+{
+    Console.WriteLine($"|куб числа {i}   |    {i * i * i,5}|");
+    i++;
+}
