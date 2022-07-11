a17ea0bf3e704209828dbb78d1b7f4be42138c02
Ildar <example.ru>
2022-07-11 23:04:28 +0500

    task042 done

---
+++ Task042/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+int TenToTwo(int n)
+{
+    if (n == 1) return 1;
+    if (n == 0) return 0;
+    return n % 2 + 10 * TenToTwo(n / 2);
+}
+
+Console.WriteLine(TenToTwo(a));
